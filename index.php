90e85fed0b2e0d80bfd22de37ac0ddb98c0d0db4
golendercaria <>
2018-06-30 10:53:31 +0200

    First commit and making first basic grid

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+		<meta charset="UTF-8">
+		<title>Grid core</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="stylesheets/grid-core.css" />
+</head>
+<body>
+	
+	<div class="d-grid grid-cols-10">
+		<div class="panel yellow col-12"></div>
+		
+		<div class="panel orange col-6"></div>
+		<div class="panel blue   col-6"></div>
+		
+		<div class="panel purple col-4"></div>
+		<div class="panel yellow col-4"></div>
+		<div class="panel green col-4"></div>
+		
+		<div class="panel orange d-grid grid-rows-3 col-12">
+			<div class="panel col-6 row-2"></div>
+			<div class="panel col-6 row-1"></div>
+			<div class="panel col-6 row-1"></div>
+			<div class="panel col-12 row-1"></div>
+		</div>
+	</div>
+
+</body>
+</html>
