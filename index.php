acbbe9ae585ed36349cb902d3604b03dc497d917
golendercaria <>
2018-06-30 11:08:58 +0200

    Responsive cols and rows

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 	<div class="d-grid grid-cols-10">
 		<div class="panel yellow col-12"></div>
 		
-		<div class="panel orange col-6"></div>
-		<div class="panel blue   col-6"></div>
+		<div class="panel orange col-6 col-s-12"></div>
+		<div class="panel blue   col-6 col-s-12"></div>
 		
-		<div class="panel purple col-4"></div>
-		<div class="panel yellow col-4"></div>
-		<div class="panel green col-4"></div>
+		<div class="panel purple col-4 col-m-12"></div>
+		<div class="panel yellow col-4 col-m-12"></div>
+		<div class="panel green col-4 col-m-12"></div>
 		
 		<div class="panel orange d-grid grid-rows-3 col-12">
 			<div class="panel col-6 row-2"></div>
